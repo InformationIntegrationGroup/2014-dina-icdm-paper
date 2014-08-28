6e69a22c785a6e03cd26a55a3ad7d8e3c63d8c7e
Shrikanth Narayanan <shrikanth2@gmail.com>
2014-08-27 18:02:29 -0700

    adding Rscripts for excel processing - the folder structure is modified to use the current directory

---
+++ ProcessingWithExcel/Rscripts/svmTesting.R
@@ -0,0 +1,35 @@
+#Testing part
+library(e1071)
+
+args<-commandArgs(TRUE)
+args
+model <- load(args[2])
+predictedFileName <- args[3]
+confusionMatrixFile <- args[4]
+
+testset<-read.csv(args[1],head=T,sep=',') #first argument URL
+numOfcol<-ncol(testset)
+
+x<-testset[,-numOfcol]
+y<-testset[,numOfcol]
+
+svm.pred <- predict(svmModel, x)
+
+testset['predicted'] <- svm.pred
+
+#testset[1:8,]
+
+confusionMatrix <- table(svm.pred, y)
+pred <- predict(svmModel, x, decision.values = TRUE)
+
+#summary(svmModel)
+#attr(pred, "decision.values")[1:8,]
+
+write.table(testset, predictedFileName, sep=',', row.names=FALSE, quote = FALSE)
+
+#plot(svm.pred)
+
+write.table(confusionMatrix,file=confusionMatrixFile,sep=',',col.names=NA, quote = FALSE)
+
+accuracy <- paste("Accuracy: ",classAgreement(confusionMatrix)$diag,sep=" ")
+accuracy
